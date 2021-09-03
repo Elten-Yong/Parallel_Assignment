bfed23ce7ec7bfa820f6268e31c704ad8d996e88
Yong-Wei-Han <yongwh-wp18@student.tarc.edu.my>
2021-09-03 23:01:27 +0800

    ywh

--- Assignment/Assignment.cpp
+++ Assignment/Assignment.cpp
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <mpi.h>
 #include <assert.h>
+#include <iomanip>
 #define NUM_THREADS 8
 #define lastNumber 300000000
 using namespace std;
@@ -145,63 +146,59 @@ int main3()
     return found;
 }
 
+int main(int argc, char** argv) {
 
-int main4(int argc, char** argv) {
-    /* Declare variables */
-    int N = 300000000; /* The positive integer under which we are finding primes */
-    int sqrtN = 0; /* The square root of N, which is stored in a variable to
-                      avoid making excessive calls to sqrt(N) */
-    int c = 0; /* Used to check the next number to be circled */
-    int m = 0; /* Used to check the next number to be marked */
-    char* list1; /* The list of numbers <= sqrtN -- if list1[x] equals 1, then x
-                   is marked.  If list1[x] equals 0, then x is unmarked. */
-    char* list2; /* The list of numbers > sqrtN -- if list2[x-L] is marked, then
-                   x is marked.  If list2[x-L] equals 0, then x is unmarked. */
-    char next_option = ' '; /* Used for parsing command line arguments */
-    int S = 0; /* A near-as-possible even split of the count of numbers above
-                  sqrtN */
-    int R = 0; /* The remainder of the near-as-possible even split */
-    int L = 0; /* The lowest number in the current process's split */
-    int H = 0; /* The highest number in the current process's split */
-    int r = 0; /* The rank of the current process */
-    int p = 0; /* The total number of processes */
-    double start_time, run_time;
-    int found = 0, sum = 0; int error;
-    /* Initialize the MPI Environment */
+    int N = 300000000;  // Limit for finding prime numbers
+    int sqrtN = 0;  // sqrtN used to eliminate multiples of it
+    int c = 0; // used in for loop (sqrtN)
+    int m = 0; // used in for loop (mark array)
+    char* list1; // list one consist of 2 to sqrtN, used to eliminate multiples
+    char* list2; // list two consist of the remaining numbers
+    int splitSize = 0; // split size in array list2
+    int remainders = 0; // remainders that are going to be assigned to the last processes
+    int lowestValue = 0; // lowest value in list2 of different processes
+    int highestValue = 0; // highest value in list2 of different processes
+    int rank = 0; // processes
+    int size = 0; // rank size
+    double start_time;
+    double run_time;
+    int found = 0; // total number of prime number found
+    int error;
+
+    //Initialize the MPI Environment
     MPI_Init(&argc, &argv);
 
-    /* Determine the rank of the current process and the number of processes */
-    MPI_Comm_rank(MPI_COMM_WORLD, &r);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    // Determine the rank of the current process and the number of processes 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-    /* Calculate sqrtN */
+    // Calculate sqrtN 
     sqrtN = (int)sqrt(N);
 
-    /* Calculate S, R, L, and H */
-
+    // Calculate splitSize, remainders, lowestValue, and highestValue 
     // Split size for different processes 
-    S = (N - (sqrtN + 1)) / p;
+    splitSize = (N - (sqrtN + 1)) / size;
 
     // Remainder size for last processes
-    R = (N - (sqrtN + 1)) % p;
+    remainders = (N - (sqrtN + 1)) % size;
 
-    // Lowest boundary in each split
-    L = sqrtN + r * S + 1;
+    // Lowest value in each split
+    lowestValue = sqrtN + rank * splitSize + 1;
 
-    // Highest boundary in each split
-    H = L + S - 1;
+    // Highest value in each split
+    highestValue = lowestValue + splitSize - 1;
 
-    // finally until the last process, responsible for the remainder
-    if (r == p - 1) {
-        H += R;
+    // Finally until the last processes,it will be responsible for the remainders
+    if (rank == size - 1) {
+        highestValue += remainders;
     }
 
-    /* Allocate memory for lists */
+    // Allocate memory for lists
     list1 = (char*)malloc((sqrtN + 1) * sizeof(char));
-    list2 = (char*)malloc((H - L + 1) * sizeof(char));
+    list2 = (char*)malloc((highestValue - lowestValue + 1) * sizeof(char));
 
-    /* Exit if malloc failed */
+    // Exit if malloc failed 
     if (list1 == NULL) {
         fprintf(stderr, "Error: Failed to allocate memory for list1.\n");
         exit(-1);
@@ -213,124 +210,123 @@ int main4(int argc, char** argv) {
 
     start_time = omp_get_wtime();
 
-    //initialize array with 1 
-    // assume all is one first 
+    //Initialize array with 1 
+    //Assume all is one first 
     //list1 is	designated	for	the	numbers 2 through sqrtN
     for (c = 2; c <= sqrtN; c++) {
         list1[c] = 1;
     }
 
     //list2 contains a section	of	the	remaining	numbers. 
-    // different processes will run the program once, each has different L and H
-    for (c = L; c <= H; c++) {
-        list2[c - L] = 1;
+    //Different processes will run the program once, each has different lowestValue and highestValue
+    for (c = lowestValue; c <= highestValue; c++) {
+        list2[c - lowestValue] = 1;
     }
 
-    /* Run through each number in list1 */
+    // Run through each number in list1 
     for (c = 2; c <= sqrtN; c++) {
-        /* If the number is marked */
+        //If the number is marked 
         if (list1[c]) {
-            /* Run through each number bigger than c in list1 */
+            // Run through the multiples
             for (m = c + c; m <= sqrtN; m += c) {
-                /* If m is a multiple of c */
-                    /* Mark m */
+                // If m is a multiple of c, mark as 0
                 list1[m] = 0;
             }
-            //===========================================
-            //=============================================
-            int temp = L;
-            /* Run through each number bigger than c in list2 */
+            int temp = lowestValue;
+            // Run through each number bigger than c in list2 
+            // While temporary value is not divisible, increment the value
+            // So that it will not eliminate the wrong value in list2
             while (temp % c != 0) {
                 temp++;
             }
 
-            for (m = temp; m <= H; m += c)
+            // Start eliminate value in list 2
+            for (m = temp; m <= highestValue; m += c)
             {
-                // m = L = 4 // m - L = 0
-                // m = 6 L = 4 // c = 2 // m- L = 2
-                list2[m - L] = 0;
+                list2[m - lowestValue] = 0;
             }
         }
     }
 
-    
-    /* If Rank 0 is the current process */
-    if (r == 0) {
-        /* Run through each of the numbers in list1 */
+
+    //If Rank 0 is the current process 
+    if (rank == 0) {
+        // Run through each of the numbers in list1 
         for (c = 2; c <= sqrtN; c++) {
-            /* If the number is unmarked */
+            // If the number is does not mark as not prime 
             if (list1[c] == 1) {
-                /* The number is prime, print it */
+                // The number is prime, print it 
                // printf("%d ", c);
                 found++;
             }
         }
 
-        /* Run through each of the numbers in list2 */
-        for (c = L; c <= H; c++) {
-            /* If the number is unmarked */
-            if (list2[c - L] == 1) {
-                /* The number is prime, print it */
+        // Run through each of the numbers in list2 
+        for (c = lowestValue; c <= highestValue; c++) {
+            // If the number is does not mark as not prime 
+            if (list2[c - lowestValue] == 1) {
+                // The number is prime, print it 
                // printf("%d ", c);
                 found++;
             }
         }
 
-        /* Run through each of the other processes */
-        for (r = 1; r <= p - 1; r++) {
+        // Run through each of the other processes
+        for (rank = 1; rank <= size - 1; rank++) {
 
-            /* Calculate L and H for r */
-            L = sqrtN + r * S + 1;
-            H = L + S - 1;
+            //Calculate lowestValue and highestValue for ranks
+            lowestValue = sqrtN + rank * splitSize + 1;
+            highestValue = lowestValue + splitSize - 1;
 
-            // assigtn to last processes
-            if (r == p - 1) {
-                H += R;
+            // Assign remianders to last processes
+            if (rank == size - 1) {
+                highestValue += remainders;
             }
 
-            /* Receive list2 from the process */
-            MPI_Recv(list2, H - L + 1, MPI_CHAR, r, 0, MPI_COMM_WORLD,
+            // Receive list2 from the processes 
+            MPI_Recv(list2, highestValue - lowestValue + 1, MPI_CHAR, rank, 0, MPI_COMM_WORLD,
                 MPI_STATUS_IGNORE);
 
-            /* Run through the list2 that was just received */
-            for (c = L; c <= H; c++) {
+            // Run through the list2 that was just received 
+            for (c = lowestValue; c <= highestValue; c++) {
 
-                /* If the number is unmarked */
-                if (list2[c - L] == 1) {
-                    /* The number is prime, print it */
+                // If the number is unmarked 
+                if (list2[c - lowestValue] == 1) {
+                    // The number is prime, print it 
                    // printf("%d ", c);
                     found++;
                 }
             }
         }
-        
-        
-        MPI_Send(&found, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        printf("\n");
 
-        /* If the process is not Rank 0 */
+        // Send the value found to rank one for printing
+        MPI_Send(&found, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        cout << "\n";
+
+        // If the process is not Rank 0 
     }
     else {
 
-        /* Send list2 to Rank 0 */
-        MPI_Send(list2, H - L + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        // Send list2 to Rank 0 
+        MPI_Send(list2, highestValue - lowestValue + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
 
     run_time = omp_get_wtime() - start_time;
 
     MPI_Barrier(MPI_COMM_WORLD);
-    if (r == 1) {
+    if (rank == 1) {
+        // Receive the value found for printing
+        MPI_Recv(&found, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        MPI_Recv(&found,  1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("\n\nTotal elapsed time: %10.6f seconds\n", run_time);
+        cout << "\n\nTotal elapsed time " << fixed << setprecision(6) << run_time << " seconds \n";
         cout << "There are total of " << found << " prime number found within " << N << "\n\n";
     }
     /* Deallocate memory for list */
-    if (r == 0) {
+    if (rank == 0) {
         free(list2);
         free(list1);
     }
-    
+
     //       MPI_Barrier(MPI_COMM_WORLD);
     /* Finalize the MPI environment */
     error = MPI_Finalize();
